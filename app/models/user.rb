3274ad6d2af01bda290bdef0daedd871829e7941
Johanna Fulghum <johanna.fulghum@gmail.com>
2017-10-02 16:37:38 -0400

    Pass specs for reset_session_token!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
 
   validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
+
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
@@ -20,6 +22,11 @@ class User < ApplicationRecord
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
 
 
 
