f6924813cc2b59f5ce981efff8690ec1592683f4
Johanna Fulghum <johanna.fulghum@gmail.com>
2017-10-02 17:07:28 -0400

    Mostly pass user controller specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
 
+    if @user.save
+      sign_in(@user)
+      redirect_to goals_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
 
